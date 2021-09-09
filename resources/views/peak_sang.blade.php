46ed5ed93d13ee83eea0d9ec127bdd35c24ced69
Maulana Imam Tantowi <maulanait007@gmail.com>
2021-09-09 08:20:31 +0700

    Update

--- resources/views/peak_sang.blade.php
+++ resources/views/peak_sang.blade.php
@@ -3,5 +3,61 @@
 @section('main')
 <div class="container p-0">
     <h1 class="fw-bold text-center p-5">UNDER CONSTRUCTION!</h1>
+    <div class="border-0 card">
+        <div class="card-body">
+            <div class="row justify-content-center">
+                <div class="col-6 col-lg-4 p-2">
+                    <div class="card">
+                        <div class="card-header text-center">
+                            <p class="fw-bold m-0 text-dark">Rasa Coklat</p>
+                        </div>
+                        <div class="card-body" style="height: 200px;">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-4 p-2">
+                    <div class="card">
+                        <div class="card-header text-center">
+                            <p class="fw-bold m-0 text-dark">Rasa Matcha</p>
+                        </div>
+                        <div class="card-body" style="height: 200px;">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-4 p-2">
+                    <div class="card">
+                        <div class="card-header text-center">
+                            <p class="fw-bold m-0 text-dark">Rasa Strawberry</p>
+                        </div>
+                        <div class="card-body" style="height: 200px;">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-4 p-2">
+                    <div class="card">
+                        <div class="card-header text-center">
+                            <p class="fw-bold m-0 text-dark">Rasa Vanilla</p>
+                        </div>
+                        <div class="card-body" style="height: 200px;">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-4 p-2">
+                    <div class="card">
+                        <div class="card-header text-center">
+                            <p class="fw-bold m-0 text-dark">Rasa Alpukat</p>
+                        </div>
+                        <div class="card-body" style="height: 200px;">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
